2e29f3cd2f8545802eb5104e99529c2bc9cd9435
Sergio Miracco <sergio.miracco@hsr.ch>
2019-12-16 08:59:23 +0100

    cosmetics: fix header file syntax

--- src/hardware/include/roboteq/channel.hpp
+++ src/hardware/include/roboteq/channel.hpp
@@ -1,5 +1,17 @@
-enum struct channel : std::uint8_t
+#ifndef BOARAI_HARDWARE_ROBOTEQ_CHANNEL_HPP
+#define BOARAI_HARDWARE_ROBOTEQ_CHANNEL_HPP
+
+#include <cstdint>
+
+namespace boarai::hardware::roboteq
 {
-  channel_01 = 1,
-  channel_02 = 2,
-}
+  enum struct channel : std::uint8_t
+  {
+
+    channel_01 = 1,
+    channel_02 = 2,
+  };
+
+}  // namespace boarai::hardware::roboteq
+
+#endif
