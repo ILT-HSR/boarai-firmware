b1cb645555e61420a2f11da714b9eb13b305b9d0
Felix Morgner <felix.morgner@hsr.ch>
2019-12-19 14:54:56 +0100

    roboteq: move command execution to command type

--- src/hardware/include/roboteq/commands.hpp
+++ src/hardware/include/roboteq/commands.hpp
@@ -1,39 +1,16 @@
 #ifndef BOARAI_HARDWARE_ROBOTEQ_COMMANDS_HPP
 #define BOARAI_HARDWARE_ROBOTEQ_COMMANDS_HPP
 
-#include <array>
-#include <cstdint>
-#include <utility>
+#include "roboteq/command.hpp"
 
-namespace boarai::hardware::roboteq
+namespace boarai::hardware::roboteq::commands
 {
-  enum struct command_argument_type : std::uint8_t
-  {
-    none,
-    boolean,
-    signed_8,
-    unsigned_8,
-    signed_16,
-    unsigned_16,
-    signed_32,
-    unsigned_32,
-  };
-
-  struct command
-  {
-    std::uint16_t id;
-    command_argument_type argument_type;
-  };
-
-  namespace commands
-  {
-    auto constexpr emergency_stop = command{0x0180, command_argument_type::none};
-    auto constexpr go_to_absolute_position = command{0x0020, command_argument_type::signed_32};
-    auto constexpr go_to_relative_position = command{0x01E0, command_argument_type::signed_32};
-    auto constexpr set_motor_speed = command{0x0040, command_argument_type::signed_32};
-    auto constexpr set_encoder_counter = command{0x0060, command_argument_type::signed_32};
-    auto constexpr set_hall_counter = command{0x0080, command_argument_type::signed_32};
-  }  // namespace commands
-}  // namespace boarai::hardware::roboteq
+  auto constexpr emergency_stop = command{0x200c, command::argument_type::none};
+  auto constexpr go_to_absolute_position = command{0x2001, command::argument_type::signed_32};
+  auto constexpr go_to_relative_position = command{0x2010, command::argument_type::signed_32};
+  auto constexpr set_motor_speed = command{0x2002, command::argument_type::signed_32};
+  auto constexpr set_encoder_counter = command{0x2003, command::argument_type::signed_32};
+  auto constexpr set_hall_counter = command{0x2004, command::argument_type::signed_32};
+}  // namespace boarai::hardware::roboteq::commands
 
 #endif
--- src/hardware/src/roboteq/driver.cpp
+++ src/hardware/src/roboteq/driver.cpp
@@ -5,142 +5,10 @@
 
 #include <modbuscpp/client.hpp>
 
-#include <algorithm>
-#include <iterator>
 #include <system_error>
-#include <type_traits>
-#include <utility>
-#include <variant>
 
 namespace boarai::hardware::roboteq
 {
-  namespace
-  {
-    /**
-     * @internal
-     *
-     * Check that a commands takes arguments
-     *
-     * @param command The command to check
-     */
-    auto command_takes_arguments(command command) -> bool
-    {
-      return command.argument_type != command_argument_type::none;
-    }
-
-    /**
-     * @internal
-     *
-     * Determine the size of a command's argument
-     */
-    auto argument_size(command command) -> int
-    {
-      switch (command.argument_type)
-      {
-      case command_argument_type::boolean:
-      case command_argument_type::signed_8:
-      case command_argument_type::unsigned_8:
-        return 1;
-      case command_argument_type::signed_16:
-      case command_argument_type::unsigned_16:
-        return 2;
-      case command_argument_type::signed_32:
-      case command_argument_type::unsigned_32:
-        return 4;
-      default:
-        return 0;
-      }
-    }
-
-    /**
-     * @internal
-     *
-     * Encode the argument of a command into a pair 16-bit unsigned integers
-     */
-    template<typename ArgumentType>
-    auto encode_command_argument(command command, ArgumentType argument)
-        -> std::variant<std::pair<std::uint16_t, std::uint16_t>, std::error_code>
-    {
-      if (argument_size(command) != sizeof(argument))
-      {
-        return make_error_code(std::errc::invalid_argument);
-      }
-
-      auto high_word = static_cast<std::uint16_t>((static_cast<std::uint32_t>(argument) >> 16) & 0xff);
-      auto low_word = static_cast<std::uint16_t>(static_cast<std::uint32_t>(argument) & 0xff);
-      return std::pair{high_word, low_word};
-    }
-
-    /**
-     * @internal
-     *
-     * Run a controller command that takes no arguments
-     *
-     * @param client The MODBUS client to run the command through
-     * @param command The command to run
-     */
-    auto execute(modbus::client & client, command command) -> std::error_code
-    {
-      if (command_takes_arguments(command))
-      {
-        return make_error_code(std::errc::invalid_argument);
-      }
-
-      return client.holding_registers(static_cast<modbus::address>(command.id), 2) = {0, 0};
-    }
-
-    /**
-     * @internal
-     *
-     * Run a controller command that takes an argument
-     *
-     * @param client The MODBUS client to run the command through
-     * @param command The command to run
-     * @param argument The argument for the command invocation
-     */
-    template<typename ArgumentType>
-    auto execute(modbus::client & client, command command, ArgumentType argument) -> std::error_code
-    {
-      if (!command_takes_arguments(command))
-      {
-        return make_error_code(std::errc::invalid_argument);
-      }
-
-      return std::visit(
-          [&](auto data) {
-            if constexpr (std::is_same_v<std::error_code, decltype(data)>)
-            {
-              return data;
-            }
-            else
-            {
-              auto [high, low] = data;
-              return client.holding_registers(static_cast<modbus::address>(command.id), 2) = {high, low};
-            }
-          },
-          encode_command_argument(command, argument));
-    }
-
-    /**
-     * @internal
-     *
-     * Run a controller command that takes an argument
-     *
-     * @param client The MODBUS client to run the command through
-     * @param command The command to run
-     * @param channel The channel to run the command on
-     * @param argument The argument for the command invocation
-     */
-    template<typename ArgumentType>
-    auto execute(modbus::client & client, command command, channel channel, ArgumentType argument) -> std::error_code
-    {
-      auto channeled_command =
-          roboteq::command{static_cast<std::uint16_t>(command.id + static_cast<std::uint16_t>(channel)), command.argument_type};
-      return execute(client, channeled_command, argument);
-    }
-
-  }  // namespace
-
   driver::driver(modbus::client & client)
       : m_client{client}
   {
@@ -148,29 +16,29 @@ namespace boarai::hardware::roboteq
 
   auto driver::emergency_stop() -> std::error_code
   {
-    return execute(m_client, commands::emergency_stop);
+    return commands::emergency_stop(m_client);
   }
 
   auto driver::go_to_position(channel channel, position position) -> std::error_code
   {
-    auto command =
-        position.type() == position_type::absolute ? commands::go_to_absolute_position : commands::go_to_relative_position;
-    return execute(m_client, command, channel, position.encoder_count());
+    return (position.type() == position_type::absolute
+                ? commands::go_to_absolute_position
+                : commands::go_to_relative_position)(m_client, position.encoder_count(), static_cast<std::uint16_t>(channel));
   }
 
   auto driver::set_motor_speed(channel channel, std::int32_t speed) -> std::error_code
   {
-    return execute(m_client, commands::set_motor_speed, channel, speed);
+    return commands::set_motor_speed(m_client, speed, static_cast<std::uint16_t>(channel));
   }
 
   auto driver::set_encoder_counter(channel channel, std::int32_t counts) -> std::error_code
   {
-    return execute(m_client, commands::set_encoder_counter, channel, counts);
+    return commands::set_encoder_counter(m_client, counts, static_cast<std::uint16_t>(channel));
   }
 
   auto driver::set_hall_counter(channel channel, std::int32_t counts) -> std::error_code
   {
-    return execute(m_client, commands::set_hall_counter, channel, counts);
+    return commands::set_hall_counter(m_client, counts, static_cast<std::uint16_t>(channel));
   }
 
 }  // namespace boarai::hardware::roboteq
