23fdd6ff06ba53e217834ca3c727620e55babe1b
Felix Morgner <felix.morgner@hsr.ch>
2020-01-30 17:14:21 +0100

    boarai: add root production launch configuration

---
+++ src/boarai/launch/production.launch.py
@@ -0,0 +1,19 @@
+import launch.actions
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+HARDWARE_LAYER = launch.actions.IncludeLaunchDescription(
+    launch.launch_description_sources.PythonLaunchDescriptionSource(
+        get_package_share_directory('boarai_hardware') + '/launch/production.launch.py'
+    )
+)
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        HARDWARE_LAYER,
+    ])
