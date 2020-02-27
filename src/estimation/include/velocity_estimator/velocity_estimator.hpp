a0d97f74e9156286ecb48208be0b1a191009f892
Felix Morgner <felix.morgner@hsr.ch>
2020-02-27 19:05:10 +0100

    estimation: start listening for drive velocities

--- src/estimation/include/velocity_estimator/velocity_estimator.hpp
+++ src/estimation/include/velocity_estimator/velocity_estimator.hpp
@@ -1,7 +1,10 @@
 #ifndef BOARAI_ESTIMATION_POSITION_ESTIMATOR_HPP
 #define BOARAI_ESTIMATION_POSITION_ESTIMATOR_HPP
 
-#include "rclcpp/rclcpp.hpp"
+#include "hardware/layer_interface.hpp"
+#include "rclcpp/node.hpp"
+#include "rclcpp/node_options.hpp"
+#include "rclcpp/subscription.hpp"
 #include "support/fmt_node.hpp"
 
 namespace boarai::estimation
@@ -12,6 +15,13 @@ namespace boarai::estimation
     using super = rclcpp::Node;
 
     explicit velocity_estimator(rclcpp::NodeOptions const & options);
+
+  private:
+    auto start_subscriptions() -> void;
+
+    auto on_drive_velocity_update(hardware::topic::drive_velocity_t::SharedPtr new_velocity) -> void;
+
+    rclcpp::Subscription<hardware::topic::drive_velocity_t>::SharedPtr m_drive_velocity_subscription{};
   };
 
 }  // namespace boarai::estimation
--- src/estimation/src/velocity_estimator/velocity_estimator.cpp
+++ src/estimation/src/velocity_estimator/velocity_estimator.cpp
@@ -4,6 +4,10 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
 
+#include <functional>
+
+using namespace std::placeholders;
+
 namespace boarai::estimation
 {
 
@@ -13,6 +17,24 @@ namespace boarai::estimation
       : fmt_node{node_name, ros_namespace, options}
   {
     log_info("position_estimator starting up");
+
+    start_subscriptions();
+  }
+
+  auto velocity_estimator::start_subscriptions() -> void
+  {
+    m_drive_velocity_subscription = create_subscription<hardware::topic::drive_velocity_t>(
+        std::string{hardware::ros_namespace} + hardware::topic::drive_velocity,
+        10,
+        std::bind(&velocity_estimator::on_drive_velocity_update, this, _1));
+  }
+
+  auto velocity_estimator::on_drive_velocity_update(hardware::topic::drive_velocity_t::SharedPtr new_velocity) -> void
+  {
+    auto [linear_velocity, angular_velocity] = new_velocity->value;
+    log_info("received drive velocity update: linear = {:.4f} m/s || angular = {:.4f} deg/s",
+             linear_velocity,
+             angular_velocity);
   }
 
 }  // namespace boarai::estimation
