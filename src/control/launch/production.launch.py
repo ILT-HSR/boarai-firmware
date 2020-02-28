5c8bd9ecdb6fb7d2a5cca03bf5dc15e499a128fb
Felix Morgner <felix.morgner@hsr.ch>
2020-02-28 10:06:24 +0100

    control: adjust launchfile to previous rename

--- src/control/launch/production.launch.py
+++ src/control/launch/production.launch.py
@@ -4,10 +4,10 @@ from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
 
-POSITION_CONTROL: ComposableNode = ComposableNode(
+VELOCITY_CONTROL: ComposableNode = ComposableNode(
     package='boarai_control',
-    node_plugin='boarai::control::position_control',
-    node_name='position_control',
+    node_plugin='boarai::control::velocity_control',
+    node_name='velocity_control',
     parameters=[]
 )
 
@@ -19,7 +19,7 @@ def generate_launch_description() -> launch.LaunchDescription:
         package='rclcpp_components',
         node_executable='component_container',
         composable_node_descriptions=[
-            POSITION_CONTROL,           
+            VELOCITY_CONTROL,           
         ],
         output='screen',
         emulate_tty=True,
