1cfb307121b4bd08a0bb59687907737358f04e6f
Felix Morgner <felix.morgner@hsr.ch>
2020-07-01 15:54:46 +0200

    build: extract common features

---
+++ cmake/Modules/AddComposableNode.cmake
@@ -0,0 +1,31 @@
+function(add_composable_node NAME)
+  file(GLOB SOURCES RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" CONFIGURE_DEPENDS "src/${NAME}/*.cpp")
+  file(GLOB HEADERS RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" CONFIGURE_DEPENDS "include/${NAME}/*.hpp" "include/${NAME}/*.h")
+
+  add_library("${NAME}" SHARED
+    ${SOURCES}
+    ${HEADERS}
+  )
+
+  target_include_directories("${NAME}" PUBLIC
+    $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+    $<INSTALL_INTERFACE:include>
+  )
+
+  target_compile_features("${NAME}" PUBLIC
+    "cxx_std_17"
+  )
+
+  set_target_properties("${NAME}" PROPERTIES
+    CXX_STANDARD_REQUIRED ON
+    CXX_EXTENSIONS OFF
+    INTERPROCEDURAL_OPTIMIZATION ON
+    INTERPROCEDURAL_OPTIMIZATION_DEBUG OFF
+  )
+
+  ament_target_dependencies("${NAME}"
+    "boarai_support"
+    "rclcpp"
+    "rclcpp_components"
+  )
+endfunction()
