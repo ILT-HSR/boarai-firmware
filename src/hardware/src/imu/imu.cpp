9c4ff3167d872eafad2334f343745fd88cd8f577
Felix Morgner <felix.morgner@hsr.ch>
2020-07-03 10:37:15 +0200

    hardware: correctly initialize IMU clock

--- src/hardware/src/imu/imu.cpp
+++ src/hardware/src/imu/imu.cpp
@@ -2,6 +2,7 @@
 #include "imu/imu.hpp"
 
 #include "imu/bno055.hpp"
+#include "rclcpp/clock.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
 #include "support/interfaces.hpp"
 
@@ -15,6 +16,7 @@ namespace boarai::hardware
 
   imu::imu(rclcpp::NodeOptions const & options)
       : fmt_node{node_name, ros_namespace, options}
+      , m_clock{std::make_shared<rclcpp::Clock>(RCL_ROS_TIME)}
   {
     try
     {
